3faceb27c4d8c130ea0d161505c190f6dae3a62f
yonetaku0519 <yonezawa051319@gmail.com>
2021-12-11 23:05:45 +0900

    getParameter test

---
+++ Intermediate_03_Sample/src/main/java/SampleServlet02.java
@@ -0,0 +1,23 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SampleServlet02")
+public class SampleServlet02 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String name = request.getParameter("name");
+		String age = request.getParameter("age");
+		
+		System.out.println("name:" + name);
+		System.out.println("age:" + age);
+		
+	}
+
+}
